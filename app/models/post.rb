dce2ee7620d93656d2d3ac0b329bed70e7184585
E-Vargo <vargoelliott@gmail.com>
2022-03-07 21:51:54 -0800

    commit tree 2629673494b1390d73232a0490688717f72c0364

--- app/models/post.rb
+++ app/models/post.rb
@@ -3,6 +3,14 @@ class Post < ActiveRecord::Base
   has_many :categories, through: :post_categories
   has_many :comments
   has_many :users, through: :comments
+  accepts_nested_attributes_for :categories
 
+  def categories_attributes=(categories_attributes) 
+    categories_attributes.values.each do |c| 
+      if !c[:name].empty?
+      self.categories << Category.find_or_create_by(name: c[:name])
+      end
+    end
+  end
 
 end
